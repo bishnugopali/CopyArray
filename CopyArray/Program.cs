7eb58dd425e1deca9e32b34af7ca0ed6261a598c
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-04 13:09:38 +0000

    copy array

--- CopyArray/Program.cs
+++ CopyArray/Program.cs
@@ -10,6 +10,31 @@ namespace CopyArray
     {
         static void Main(string[] args)
         {
+            //a program in C# Sharp to copy the elements one array into another array. 
+
+            int i, n;
+            int[] a = new int[100];
+            int[] b = new int[100];
+            Console.Write("Input the number of elements to store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Input {0} number of elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                b[i] = a[i];
+            }
+            Console.Write("Second array after first array is copied: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", b[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
